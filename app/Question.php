9cd90570335a1bc073c8893fda29351a8fa2bf27
Surajit Basa <surajitbasak109@gmail.com>
2019-09-20 01:58:25 +0530

    Prepopulate users and questions table using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
@@ -11,5 +12,11 @@ class Question extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
     }
 }
