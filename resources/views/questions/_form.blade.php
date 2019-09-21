77facc60a7ec23dafd09b4dbaeb1ee8fad14de30
Surajit Basa <surajitbasak109@gmail.com>
2019-09-22 01:01:57 +0530

    Extend the form functionality to be able to update question and applied autosize on textarea

---
+++ resources/views/questions/_form.blade.php
@@ -0,0 +1,28 @@
+@csrf
+
+<div class="form-group">
+    <label for="question-title">Question Title</label>
+    <input type="text" name="title" id="question-title" value="{{ old('title', $question->title) }}"
+        class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+
+    @if ($errors->has('title'))
+    <div class="invalid-feedback">
+        <strong>{{ $errors->first('title') }}</strong>
+    </div>
+    @endif
+</div>
+
+<div class="form-group">
+    <label for="question-body">Ask your Question</label>
+    <textarea name="body" id="question-body" rows="5"
+        class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body', $question->body) }}</textarea>
+    @if ($errors->has('body'))
+    <div class="invalid-feedback">
+        <strong>{{ $errors->first('body') }}</strong>
+    </div>
+    @endif
+</div>
+
+<div class="form-group">
+    <button type="submit" class="btn btn-outline-primary btn-lg">{{ $buttonText }}</button>
+</div>
